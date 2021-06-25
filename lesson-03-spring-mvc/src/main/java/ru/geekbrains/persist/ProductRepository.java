3a3b29875f6c3dc73b9ddddafe0be6f00696219c
Pavel A. Khanin <pkhanyn@gmail.com>
2021-06-25 14:59:50 +0300

    lesson3

---
+++ lesson-03-spring-mvc/src/main/java/ru/geekbrains/persist/ProductRepository.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.persist;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ProductRepository {
+
+    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
+
+    private final AtomicLong identity = new AtomicLong(0);
+
+    public void addProduct(Product product) {
+        if (product.getId() == null) {
+            product.setId(identity.incrementAndGet());
+        }
+        productMap.put(product.getId(), product);
+    }
+
+    @PostConstruct
+    public void initRepostory() {
+        this.addProduct(new Product("Товар1", 12.4f));
+        this.addProduct(new Product("Товар2", 12.4f));
+        this.addProduct(new Product("Товар3", 12.4f));
+    }
+
+    public Product getProductById(Long id) {
+        return productMap.get(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    public void deleteProductById(Long id) {
+        productMap.remove(id);
+    }
+/*
+    public void updateProduct(Product product) {
+        productMap.put(product.getId(), product);
+    }
+
+ */
+}
